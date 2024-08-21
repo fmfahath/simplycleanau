622dcac14a11e9918cfc5ab504732bb403c7eab0
fmfahath <fmfahath.dev@gmail.com>
2024-08-22 04:27:56 +0530

    nabvar AOS, toggle button show/hide function added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,30 @@
+//AOS animation--------------------------------------
+AOS.init();
+
+// hide navbar---------------------------------------
+document.addEventListener('scroll', () => {
+    const navbar = document.querySelector('#navbar');
+
+    if (window.scrollY > 20) {
+        navbar.classList.add('navbar-hide')
+    } else {
+        navbar.classList.remove('navbar-hide')
+    }
+})
+
+
+// hide toggle menu after click on nav link------------
+const navItems = document.querySelector(".navbar-nav")
+const navbarToggler = document.querySelector(".navbar-toggler")
+const navbarCollapse = document.querySelector(".navbar-collapse")
+
+navItems.addEventListener('click', () => {
+    console.log('triggered')
+    navbarToggler.classList.add("collapsed")
+    navbarCollapse.classList.remove("show")
+})
+
+
 //scroll-to-top button------------------------------------
 document.addEventListener('scroll', () => {
     const toTopButton = document.querySelector('#to-top');
@@ -9,46 +36,16 @@ document.addEventListener('scroll', () => {
     }
 })
 
-// counter up--------------------------------------------
-// const counters = document.querySelectorAll(".counters span");
-// const container = document.querySelector(".container");
 
-// let activated = false
 
-// window.onscroll = () => {
-//     if (!activated) {
-//         if (window.scrollY >= container.offsetTop) {
-//             counters.forEach(counter => {
-//                 let start = 0;
-//                 let end = counter.dataset.count
-//                 // console.log(end)
-
-//                 let count = setInterval(() => {
-//                     start++;
-//                     counter.textContent = start;
-//                     if (start == end) {
-//                         clearInterval(count);
-//                     }
-//                 }, 2000 / end)
-//             })
-//         }
-//         activated = true;
-//     }
-// }
-
-// -----------------------------------------------------------------------------
+// Counterup-----------------------------------------------------------------------------
 let activated = false
 const counters = document.querySelectorAll(".counters span");
 const container = document.querySelector(".container");
 
 document.addEventListener("scroll", () => {
 
-    console.log('counter triggered')
-    console.log('state: ', activated)
-    console.log('counter scroll:', window.scrollY)
-
     if (!activated && window.scrollY > 200) {
-        console.log("conter in");
 
         counters.forEach(counter => {
             let start = 0;
@@ -68,11 +65,3 @@ document.addEventListener("scroll", () => {
 })
 
 // -------------------------------------------------------------------------
-
-// const innerWidth = window.innerWidth;
-// const docElmWidth = document.documentElement.clientWidth;
-// const docbdyWidth = document.body.clientWidth;
-
-// console.log(innerWidth, docElmWidth, docbdyWidth)
-
-// alert('Current scroll from the top: ' + window.scrollY);
